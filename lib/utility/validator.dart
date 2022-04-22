a43d7f697726d66281173d4e92e71ababc09a0dc
Tifany Angelia <tifanyangelia@Tifanys-MacBook-Air.local>
2022-04-22 12:22:25 +0700

    fix validator password

--- lib/utility/validator.dart
+++ lib/utility/validator.dart
@@ -2,5 +2,5 @@ class Validator {
   static RegExp validatorEmail = RegExp(
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
   static RegExp validatorPassword =
-      RegExp(r"^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]{6,}$");
+      RegExp(r"^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d\w\W]{6,}$");
 }
